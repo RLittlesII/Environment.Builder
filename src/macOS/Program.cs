dea772a7d5ac4018e8deacbe3636ea456a9ed16e
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2022-08-18 23:36:49 -0500

    fix: add Spectre.IO registrations

--- src/macOS/Program.cs
+++ src/macOS/Program.cs
@@ -1,11 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using Cupboard;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.IO;
 
 Console.WriteLine("Hello, World!");
 
 return CupboardHost
     .CreateBuilder()
     .AddCatalog<MobileDevelopmentCatalog>()
+    .ConfigureServices(services =>
+        services
+            .AddSingleton<IEnvironment, Spectre.IO.Environment>()
+            .AddSingleton<IPlatform, Platform>())
     .Build()
     .Run(args);
